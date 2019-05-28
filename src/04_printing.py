6790e7c0e78a0dd2a21a866f24a344e4f02bde39
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-05-28 15:39:28 -0400

    add complete exercise 04

--- src/04_printing.py
+++ src/04_printing.py
@@ -14,6 +14,7 @@ z = "I like turtles!"
 print("x is %d, y is %0.2f, z is \"%s\"" % (x, y, z))
 
 # Use the 'format' string method to print the same thing
+print("x is {}, y is {}, z is \"{}\"".format(x, round(y, 2), z))
 
 # Finally, print the same thing using an f-string
-print(f"x is {x}, y is {y:.3}, z is {z}")
+print(f"x is {x}, y is {round(y,2)}, z is \"{z}\"")
